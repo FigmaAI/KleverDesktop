51df07e4a8014d47c9bf30725eb412f98a51d8f3
JooHyung Park <dusskapark@gmail.com>
2025-07-02 23:32:30 +0800

    Remove useless scripts

--- app/src/main/kotlin/com/klever/desktop/server/models/AzureModel.kt
+++ app/src/main/kotlin/com/klever/desktop/server/models/AzureModel.kt
@@ -121,7 +121,7 @@ class AzureModel(
                             5. Do not modify the structure of the text
                             
                             Example:
-                            Input: "Click here to 새로운 메시지를 작성하세요 and send"
+                            Input: "Haz clic aquí para escribir un nuevo mensaje y enviar"
                             Output: "Click here to write new message and send"
                         """.trimIndent())
                     }
